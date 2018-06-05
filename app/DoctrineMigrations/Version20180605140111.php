6dd02e57ac75a909b936dd10ac4db7f10e17285a
Igor <igor.grozovskiy@gmail.com>
2018-06-05 17:01:31 +0300

    Fix migrations

--- app/DoctrineMigrations/Version20180605140111.php
+++ app/DoctrineMigrations/Version20180605140111.php
@@ -8,7 +8,7 @@ use Doctrine\DBAL\Schema\Schema;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-class Version20180601150333 extends AbstractMigration
+class Version20180605140111 extends AbstractMigration
 {
     /**
      * @param Schema $schema
@@ -18,7 +18,7 @@ class Version20180601150333 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('ALTER TABLE user ADD usersurname VARCHAR(255) NOT NULL');
+        $this->addSql('CREATE TABLE user (id INT AUTO_INCREMENT NOT NULL, username VARCHAR(255) NOT NULL, full_name VARCHAR(255) DEFAULT NULL, email VARCHAR(255) NOT NULL, password VARCHAR(255) NOT NULL, role VARCHAR(255) DEFAULT \'ROLE_USER\', salt VARCHAR(255) DEFAULT NULL, registration_date DATETIME DEFAULT NULL, listings SMALLINT DEFAULT NULL, reviews SMALLINT DEFAULT NULL, seo_rank SMALLINT DEFAULT NULL, messages SMALLINT DEFAULT NULL, instagram SMALLINT DEFAULT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB');
     }
 
     /**
@@ -29,6 +29,6 @@ class Version20180601150333 extends AbstractMigration
         // this down() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('ALTER TABLE user DROP usersurname');
+        $this->addSql('DROP TABLE user');
     }
 }
