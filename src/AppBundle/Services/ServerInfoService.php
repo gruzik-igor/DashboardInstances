0ff68e0f034cba6ffa6d3d15d286e5be39f01987
Igor <igor.grozovskiy@gmail.com>
2018-06-05 17:33:28 +0300

    Add meminfo

--- src/AppBundle/Services/ServerInfoService.php
+++ src/AppBundle/Services/ServerInfoService.php
@@ -5,14 +5,19 @@ namespace AppBundle\Services;
 class ServerInfoService 
 {
     public function getSystemMemInfo() 
-    {       
-        $data = explode("\n", file_get_contents("/proc/meminfo"));
-        $meminfo = array();
-        foreach ($data as $line) {
-            list($key, $val) = explode(":", $line);
-            $meminfo[$key] = trim($val);
-        }
+    {   
+        $free = shell_exec('free');
+        $free = (string)trim($free);
+        $free_arr = explode("\n", $free);
+        $mem = explode(" ", $free_arr[1]);
+        $mem = array_filter($mem);
+        $mem = array_merge($mem);
+        $meminfo['MemTotal'] = $mem[2] + $mem[1] / 1024;
+        $meminfo['MemFree'] = $mem[2] / 1024;
+
+
         return $meminfo;
+        
     }
 }
 
