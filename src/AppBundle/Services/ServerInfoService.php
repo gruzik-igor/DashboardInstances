d2d24989a9e8cfaf0373dad7c183872a7ad97415
Igor <igor.grozovskiy@gmail.com>
2018-06-06 17:00:13 +0300

    Fix cpuinfo

--- src/AppBundle/Services/ServerInfoService.php
+++ src/AppBundle/Services/ServerInfoService.php
@@ -30,13 +30,43 @@ class ServerInfoService
         return $diskInfo;
         
     }
+    public function GetCoreInformation() {
+        $data = file('/proc/stat');
+        $cores = array();
+        foreach( $data as $line ) {
+            if( preg_match('/^cpu[0-9]/', $line) )
+            {
+                $info = explode(' ', $line );
+                $cores[] = array(
+                    'user' => $info[1],
+                    'nice' => $info[2],
+                    'sys' => $info[3],
+                    'idle' => $info[4]
+                );
+            }
+        }
+        return $cores;
+    }
 
-    function getSystemCpuInfo($coreCount = 1, $interval = 1) {
-        $rs = sys_getloadavg();
-        $interval = $interval >= 1 && 3 <= $interval ? $interval : 1;
-        $load = $rs[$interval];
-        $cpuinfo = $load * 100;
-       // $cpuinfo = round(($load * 100) / $coreCount,2);
+    var $stat1 = GetCoreInformation();
+    var $stat1 = GetCoreInformation();
+
+    public function getSystemCpuInfo($stat1, $stat2) {
+        if( count($stat1) !== count($stat2) ) {
+            return;
+        }
+        $cpuinfo = array();
+        for( $i = 0, $l = count($stat1); $i < $l; $i++) {
+            $dif = array();
+            $dif['user'] = $stat2[$i]['user'] - $stat1[$i]['user'];
+            $dif['nice'] = $stat2[$i]['nice'] - $stat1[$i]['nice'];
+            $dif['sys'] = $stat2[$i]['sys'] - $stat1[$i]['sys'];
+            $dif['idle'] = $stat2[$i]['idle'] - $stat1[$i]['idle'];
+            $total = array_sum($dif);
+            $cpu = array();
+            foreach($dif as $x=>$y) $cpu[$x] = round($y / $total * 100, 1);
+            $cpuinfo['cpu' . $i] = $cpu;
+        }
         return $cpuinfo;
     }
 }
