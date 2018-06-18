ea3f5d50e1a1ac1c3a298038a9633992d7153023
Igor <igor.grozovskiy@gmail.com>
2018-06-18 11:34:34 +0300

    Fix sys info

--- src/AppBundle/Services/ServerInfoService.php
+++ src/AppBundle/Services/ServerInfoService.php
@@ -63,7 +63,6 @@ class ServerInfoService
         $new_array = array();
         array_walk($sysinfo,  function ($val, $key, &$new_array){
             $nums = explode(': ',$val);
-            var_dump($nums); die;
             $new_array[trim($nums[0])] = trim($nums[1]);
 
         } , $new_array);
