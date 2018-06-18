de2ac5fe897e62d0ec13bc274adccd96c4dc404c
Igor <igor.grozovskiy@gmail.com>
2018-06-18 18:42:19 +0300

    Fix sys info

--- src/AppBundle/Services/ServerInfoService.php
+++ src/AppBundle/Services/ServerInfoService.php
@@ -65,7 +65,7 @@ class ServerInfoService
         //$new_array = array();
         
 
-        while (list ($key, $val) = each ($sysinfo) ) echo'<pre>'; echo $val; echo'<pre>';
+        while (list ($key, $val) = each ($sysinfo) ) echo'<pre>'; echo $val; echo'<pre>'; die;
         
         
 
