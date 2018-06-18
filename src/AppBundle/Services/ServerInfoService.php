cf8ece1fbd5e523b03117183308c32bcfa22dd44
Igor <igor.grozovskiy@gmail.com>
2018-06-18 18:48:32 +0300

    Fix sys info

--- src/AppBundle/Services/ServerInfoService.php
+++ src/AppBundle/Services/ServerInfoService.php
@@ -65,7 +65,7 @@ class ServerInfoService
         //$new_array = array();
         
 
-        while (list ($key, $val) = each ($sysinfo) ) echo $n_arr = explode(': ',$val).'<br>'; 
+        while (list ($key, $val) = each ($sysinfo) ) var_dump($n_arr = explode(': ',$val)).'<br>'; 
         
         
 
