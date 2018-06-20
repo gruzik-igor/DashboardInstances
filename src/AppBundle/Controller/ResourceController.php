f4a83b5f860b7354eeb0fdbd2958ea2763188bcb
Igor <igor.grozovskiy@gmail.com>
2018-06-20 12:24:43 +0300

    Resource page

--- src/AppBundle/Controller/ResourceController.php
+++ src/AppBundle/Controller/ResourceController.php
@@ -2,6 +2,7 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Entity\Resource;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
@@ -17,9 +18,33 @@ class ResourceController extends BaseController
      */
     public function resourcesAction(Request $request)
     {
-        return $this->render('@App/resources/index.html.twig',
+        $response = $this->render('@App/resources/index.html.twig',
             [
                 'resources' => $this->findBy('AppBundle:Resource', [])
             ]);
+
+        if ($request->getMethod() === 'POST') {
+            $em = $this->getDoctrine()->getManager();
+
+            $repository = $this->getRepository('AppBundle:Resource');
+
+            $resources = $request->request->get('resources');
+
+            foreach ($resources as $item) {
+                $resource = $repository->findOneBy(['name' => $item['resourceName']]);
+
+                if ($resource instanceof Resource) {
+                    $resource->setDefaultValue($item['defaultValue']);
+
+                    $em->persist($resource);
+                }
+            }
+
+            $em->flush();
+
+            $response = $this->redirectToRoute('dashboard');
+        }
+
+        return $response;
     }
 }
