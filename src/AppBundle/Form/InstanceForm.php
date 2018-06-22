2e14dedb108d77c9fa62d2b8b2874b24ce4865f8
Igor <igor.grozovskiy@gmail.com>
2018-06-22 18:26:17 +0300

    create instance form

--- src/AppBundle/Form/InstanceForm.php
+++ src/AppBundle/Form/InstanceForm.php
@@ -4,7 +4,8 @@ namespace AppBundle\Form;
 
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
-use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+
 
 class InstanceFormForm extends AbstractType
 {
@@ -14,10 +15,10 @@ class InstanceFormForm extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('name')
-            ->add('resourse_limits_cpu')
-            ->add('resourse_limits_ram')
-            ->add('resourse_limits_hdd');
+            ->add('name',TextType::class)
+            ->add('resourse_limits_cpu',TextType::class)
+            ->add('resourse_limits_ram',TextType::class)
+            ->add('resourse_limits_hdd',TextType::class);
  
     }
 
