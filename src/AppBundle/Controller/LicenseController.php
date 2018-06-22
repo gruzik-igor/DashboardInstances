5ba441a03191ad2d16af5a3ee70d57fa37bb0a6e
Igor <igor.grozovskiy@gmail.com>
2018-06-22 12:03:33 +0300

    Change license default value

---
+++ src/AppBundle/Controller/LicenseController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use AppBundle\Entity\License;
+use AppBundle\Entity\Resource;
+use AppBundle\Form\LicenseForm;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use AppBundle\Form\UserForm;
+use AppBundle\Entity\User;
+
+
+class LicenseController extends BaseController
+{
+    /**
+     * @Route("/licenses/{license}", name="edit-license")
+     */
+    public function editAction(License $license, Request $request)
+    {
+        $form = $this->createForm(LicenseForm::class, $license);
+        $form->handleRequest($request);
+
+        if ($request->getMethod() === 'POST' && $form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($license);
+            $em->flush();
+        }
+
+        return $this->redirectToRoute('dashboard');
+    }
+}
---
+++ src/AppBundle/Form/LicenseForm.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace AppBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class LicenseForm extends AbstractType
+{
+
+    
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('issued')
+            ->add('usage')
+            ->add('rate');
+ 
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'AppBundle\Entity\License',
+            'allow_extra_fields' => true,
+        ));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getBlockPrefix()
+    {
+        return 'appbundle_license';
+    }
+}
