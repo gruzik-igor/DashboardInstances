702fd370c997703283aad7eb65cca8beea12a5fd
Igor <igor.grozovskiy@gmail.com>
2018-07-02 11:07:11 +0300

    LicenseForm

--- src/AppBundle/Form/InstanceForm.php
+++ src/AppBundle/Form/InstanceForm.php
@@ -4,10 +4,10 @@ namespace AppBundle\Form;
 
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\OptionsResolver\OptionsResolver;
 
 
-class InstanceFormForm extends AbstractType
+class InstanceForm extends AbstractType
 {
     /**
      * {@inheritdoc}
