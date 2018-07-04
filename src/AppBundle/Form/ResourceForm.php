eed9b2413faa6d7cca45f0c95b482fc582db6184
Igor <igor.grozovskiy@gmail.com>
2018-07-04 11:41:52 +0300

    LicenseForm

--- src/AppBundle/Form/ResourceForm.php
+++ src/AppBundle/Form/ResourceForm.php
@@ -3,6 +3,7 @@
 namespace AppBundle\Form;
 
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CollectionType;
 use Symfony\Component\Form\Extension\Core\Type\NumberType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
