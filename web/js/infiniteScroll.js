ac578895736e8735a2a1bfb5230d7ad15b410a05
Igor <igor.grozovskiy@gmail.com>
2018-07-10 13:08:36 +0300

    Request license

---
+++ web/js/infiniteScroll.js
@@ -0,0 +1,31 @@
+let is_loading = false;
+
+$(function () {
+    $(window).scroll(function () {
+        if ($(window).scrollTop() + $(window).height() == $(document).height()) {
+            if (is_loading == false) {
+                is_loading = true;
+                var infiniteScrollBody = $('#infiniteScrollBody');
+
+                var currentLimit = infiniteScrollBody.attr('data-current-limit');
+
+                //ajaxSpinnerShow($('#loader'));
+
+                var url = infiniteScrollBody.attr('data-url');
+                var method = infiniteScrollBody.attr('data-method');
+                $.ajax({
+                    url: url + '?_limit=' + (currentLimit + 1),
+                    type: method,
+                    success: function (data, status, reqeust) {
+                        infiniteScrollBody.attr('data-current-limit', (currentLimit + 1);
+
+                        infiniteScrollBody.append(data);
+
+
+                        is_loading = false;
+                    }
+                });
+            }
+        }
+    });
+});
