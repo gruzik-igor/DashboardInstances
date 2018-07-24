ff939fccee38ebe3c6ebd646d7532dcc9e8806d5
Ihor Hrozovskyi <ihor.hrozovskyi@devcom.com>
2018-07-24 17:42:16 +0300

    create bash script

---
+++ src/AppBundle/Controller/DeployInstanceController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use AppBundle\Controller\BaseController;
+use AppBundle\Entity\Instance;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\BrowserKit\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
+
+class DeployInstanceController extends BaseController
+{
+    /**
+     * @Route("/deploy-instance/{instance}", name="deploy-instance")
+     * @Security("has_role('ROLE_SUPER_ADMIN')")
+     */
+    public function deployInstanceAction(Instance $instance)
+    {
+        $instanceName = $instance->getName() .'_'. $instance->getId();
+        $dbname = 'sf_' . strtolower($instance->getName());
+        $new_user = strtolower($instance->getName()).'_admin';
+        $host = '%';
+        $new_db_pw = strtolower($instance->getName()).$instance->getId().'QxY37f';
+
+        $output = shell_exec($this->getParameter('web_dir') . '/deploy/./deploy.sh ' . $instanceName . ' ' . $dbname . ' '. $new_user . ' ' .$host.' '.$new_db_pw.'> /dev/null 2>&1 &');
+
+        return $this->redirectToRoute('dashboard');
+    }
+
+
+}
