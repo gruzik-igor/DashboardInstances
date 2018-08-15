d0b9ea77cc8778108b81744aa065449bf3ab9601
Ihor Hrozovskyi <ihor.hrozovskyi@devcom.com>
2018-08-15 11:10:48 +0300

    install docker library

---
+++ app/DoctrineMigrations/Version20180727085429.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Application\Migrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20180727085429 extends AbstractMigration
+{
+    /**
+     * @param Schema $schema
+     */
+    public function up(Schema $schema)
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE instance ADD deploying_status VARCHAR(255) NOT NULL, ADD deploying_date DATE DEFAULT NULL');
+        $this->addSql('ALTER TABLE user CHANGE role role VARCHAR(255) DEFAULT \'ROLE_SUPER_ADMIN\'');
+    }
+
+    /**
+     * @param Schema $schema
+     */
+    public function down(Schema $schema)
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE instance DROP deploying_status, DROP deploying_date');
+        $this->addSql('ALTER TABLE user CHANGE role role VARCHAR(255) DEFAULT \'ROLE_USER\' COLLATE utf8_unicode_ci');
+    }
+}
--- src/AppBundle/Controller/InstanceController.php
+++ src/AppBundle/Controller/InstanceController.php
@@ -13,6 +13,12 @@ use AppBundle\Form\UserForm;
 use AppBundle\Entity\User;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
 
+use Docker\Stream\DockerRawStream;
+use Docker\API\Model\ContainerConfig;
+use Docker\API\Model\ContainersCreatePostBody;
+use Docker\DockerClientFactory;
+use Docker\Context\ContextBuilder;
+use Docker\Docker;
 
 class InstanceController extends BaseController
 {
@@ -38,14 +44,61 @@ class InstanceController extends BaseController
         return $response;
     }
 
+
+    private static $docker;
+
+    public static function getDocker(): Docker
+    {
+        if (null === self::$docker) {
+            self::$docker = Docker::create();
+        }
+        return self::$docker;
+    }
+
+    private function getManager()
+    {
+        return self::getDocker();
+    }
+
+    /**
+     * @Route("/instance/{instance}", name="docker-status")
+     * @Security("has_role('ROLE_SUPER_ADMIN')")
+     */
+
+    public function dockerAction()
+    {
+        $docker = Docker::create();
+        $containerConfig = new ContainersCreatePostBody();
+        $containerConfig->setHostname( 'boomrev');
+        $containerConfig->setImage('busybox:latest');
+        $containerConfig->setDomainname( 'boomrev');
+        $containerConfig->setCmd(['echo', 'I am running a command']);
+        var_dump($containerConfig);die;
+        $containerLogsResult = $docker->getContainerManager()->logs('23b0da3fff80', ["stderr"=>true, "stdout"=>true]);
+        $containerCreateResult = $docker->containerCreate($containerConfig);
+        var_dump($containerCreateResult);die;
+        $containerCreateResult = $docker->containerCreate($containerConfig);
+
+
+        //$docker->containerStart($containerCreateResult->getId());
+        //$docker->containerStart('boomrev');
+        var_dump($containerCreateResult);die;
+//        $containers = $docker->containerList();
+//        var_dump($containers);die;
+//        foreach ($containers as $container) {
+//            var_dump($container->getNames());die;
+//        }
+
+    }
+
     /**
      * @Route("/instance/{instance}", name="change-status")
      * @Security("has_role('ROLE_SUPER_ADMIN')")
      */
     public function statusAction(Instance $instance, Request $request)
     {
-        $em = $this->getDoctrine()->getManager();
 
+        $em = $this->getDoctrine()->getManager();
         $instance->setStatus($request->query->get('status'));
         $em->persist($instance);
         $em->flush();
